f4514a7abffb107ed83ac9f04230f7e6c8a5d3f3
osheaoshea <78743537+osheaoshea@users.noreply.github.com>
2024-09-18 13:18:24 +0100

    add analytics back in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4JBBEHZRHH"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4JBBEHZRHH');
+          `}
+        </Script>
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -45,18 +57,6 @@ export default function RootLayout({
           <Footer />
           <div className="absolute inset-0 bg-grid-black/[0.035] mask-radial-faded -z-10"></div>
         </div>
-        {/* <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-4JBBEHZRHH"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-4JBBEHZRHH');
-          `}
-        </Script> */}
       </body>
     </html>
   );
