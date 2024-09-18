550951f04f559c7f82a6d8eccf509e5e64f18ef2
osheaoshea <78743537+osheaoshea@users.noreply.github.com>
2024-09-18 12:53:00 +0100

    add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={'{inter.className} bg-gray-50'}>
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MDFJB8Z8');
+          `}
+        </Script>
+      </head>
+      <body className={"{inter.className} bg-gray-50"}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MDFJB8Z8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <div className="flex flex-col min-h-screen">
           <Navbar />
           {children}
           <Footer />
           <div className="absolute inset-0 bg-grid-black/[0.035] mask-radial-faded -z-10"></div>
         </div>
-        <Script
+        {/* <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-4JBBEHZRHH"
           strategy="afterInteractive"
         />
@@ -37,7 +56,7 @@ export default function RootLayout({
             gtag('js', new Date());
             gtag('config', 'G-4JBBEHZRHH');
           `}
-        </Script>
+        </Script> */}
       </body>
     </html>
   );
