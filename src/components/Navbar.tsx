336640da331726091d135ab0bd2fc3ebcd9b7c3b
Jake <41425888+t3nn3t@users.noreply.github.com>
2024-10-04 12:19:39 +0100

    yo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,20 @@ import Image from "next/image";
 export const Navbar = () => {
   return (
     <header className="bg-transparent relative z-50">
-      <div className="flex max-w-6xl mx-auto py-6 px-8 justify-center lg:justify-between">
-        <div className="flex items-center space-x-2 text-2xl font-bold text-gray-800">
+      <div className="flex max-w-6xl mx-auto py-4 px-8 justify-center lg:justify-between">
+        <div className="flex items-center space-x-2 text-2xl text-gray-800">
           <span>
             <Image
               src="/img/blueside.svg"
               alt="BlueSide"
               width="32"
               height="32"
-              className="w-36"
+              className="w-52"
             />
           </span>
+          <p className="text-sm text-center leading-normal text-blueside-dark laptop:text-lg sm:text-base lg:w-5/6 mt-5 mb-4 px-12">
+            Product
+          </p>
         </div>
       </div>
     </header>
