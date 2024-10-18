baeeae48aa29c13ce53133d1d0411a4c3779f381
Jake <41425888+t3nn3t@users.noreply.github.com>
2024-10-18 14:46:26 +0100

    added microsoft founder center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { FeatureOne } from "@/components/FeatureOne";
-import { LogoBanner } from "@/components/LogoBanner";
 import { Hero } from "@/components/Hero";
 import { VideoPanel } from "@/components/VideoPanel";
 import { DemoVideo } from "@/components/DemoVideo";
@@ -7,13 +6,14 @@ import { FeatureLights } from "@/components/FeatureLights";
 import { FeatureRag } from "@/components/FeatureRag";
 import { FeatureAction } from "@/components/FeatureAction";
 import Faq from "@/components/Faq";
+import { Banner } from "@/components/Banner";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen max-w-7xl mx-auto px-6 sm:px-8">
       <Hero />
       <VideoPanel />
-      <LogoBanner />
+      <Banner />
       <FeatureLights />
       <FeatureAction />
       <FeatureRag />
--- src/components/PartnerBanner.tsx
+++ src/components/PartnerBanner.tsx
@@ -2,15 +2,16 @@ import Image from "next/image";
 
 export const PartnerBanner = () => {
   return (
-    <div className="flex flex-col items-center gap-y-2 mt-8 mb-10 lg:mb-16">
-      <p className="text-base text-center font-[375] leading-normal text-blueside-dark lg:text-lg mb-6">
-        Partners with
+    <div className="flex flex-col items-center gap-y-2 mt-8 mb-10 px-4 lg:mb-16">
+      <p className="text-base text-center font-[350] leading-normal text-blueside-dark lg:text-lg mb-6">
+        Partnered with
       </p>
       <Image
-        src="/img/models_blue.png"
-        alt="OpenAI, Claude, Gemini"
+        src="/img/foundershub.png"
+        alt="Microsoft Founders Hub for Startups"
         width={400}
         height={400}
+        className="lg:w-[250px] w-[200px] md:w-[215px]"
       />
     </div>
   );
