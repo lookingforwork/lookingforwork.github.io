761f96034f375b053cdaf42dc2d419fee2e63961
osheaoshea <78743537+osheaoshea@users.noreply.github.com>
2024-10-21 19:18:12 +0100

    support page

--- src/components/support/SupportFaq.tsx
+++ src/components/support/SupportFaq.tsx
@@ -1,53 +1,8 @@
 "use client";
-import React, { useState, useRef, useEffect } from "react";
-import { CtaButton } from "./CtaButton";
+import React from "react";
+import { FaqItem } from '../Faq';
 
-interface FaqItemProps {
-  question: string;
-  answer: React.ReactNode;
-}
-
-export const FaqItem: React.FC<FaqItemProps> = ({ question, answer }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const contentRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (contentRef.current) {
-      contentRef.current.style.maxHeight = isOpen
-        ? `${contentRef.current.scrollHeight}px`
-        : "0px";
-    }
-  }, [isOpen]);
-
-  return (
-    <div className="border-b border-transparent">
-      <button className="text-left w-full" onClick={() => setIsOpen(!isOpen)}>
-        <div className="flex justify-between items-center max-w-[535px] py-4">
-          <span className="text-blueside-dark font-subtitle">{question}</span>
-          <span className="text-blueside-dark text-xl transition-transform duration-300 ease-in-out">
-            {isOpen ? "−" : "+"}
-          </span>
-        </div>
-      </button>
-      <div
-        ref={contentRef}
-        className="overflow-hidden transition-all duration-300 ease-in-out"
-        style={{ maxHeight: "0px" }}
-      >
-        <div className="pb-4 max-w-[535px] text-blueside-dark text-sm font-light">
-          {answer}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export interface FaqItem {
-  question: string;
-  answer: React.ReactNode;
-}
-
-const Faq: React.FC = () => {
+const SupportFaq: React.FC = () => {
   const faqs: FaqItem[] = [
     {
       question: "What does BlueSide do?",
@@ -99,17 +54,14 @@ const Faq: React.FC = () => {
 
   return (
     <div className="flex flex-col shrink text-blueside-dark p-6 rounded-lg">
-      <h2 className="text-3xl font-bold mb-6 mt-16">FAQs answered</h2>
+      <h2 className="text-3xl font-bold mb-6 mt-8">FAQs answered</h2>
       <div className="space-y-2">
         {faqs.map((faq, index) => (
           <FaqItem key={index} question={faq.question} answer={faq.answer} />
         ))}
       </div>
-      <div className="flex justify-center my-16">
-        <CtaButton />
-      </div>
     </div>
   );
 };
 
-export default Faq;
+export default SupportFaq;
